8972d9aae3aa608bf15e2c9ea75e0c6b2d690f4f
stinface98 <austin.atmaja@gmail.com>
2018-07-03 20:52:45 -0400

    added mel2frq method and function to convert channels in mels to channels in Hz

--- Phase2.m
+++ Phase2.m
@@ -14,7 +14,7 @@ function Phase2(fileName, extension)
     sound(y, Fs);
 
     % 3.4
-    audiowrite(strcat('mono',fileName, '.wav'), y, Fs);
+    %audiowrite(strcat('mono',fileName, '.wav'), y, Fs);
 
     % 3.5
     %plotting audio as a function of sample num
@@ -50,9 +50,16 @@ function Phase2(fileName, extension)
     %Task 4 - Filter Design
     
     %Step A: Determining the frequency bands
+    %Determine upper and lower bounds of human hearing in mels (unit of
+    %pitch)
     melsLowerBound = frq2mel(100);
     melsUpperBound = frq2mel(8000);
+    
+    %Divide pitches into 8 channels of even width
     melsChannels = linspace(melsLowerBound, melsUpperBound, 9);
+    
+    %Convert channels from pitches (mels) to frequency (Hz)
+    freqChannels = mel2frq(melsChannels);
     
     %Task 5 - Filter the sound
     
